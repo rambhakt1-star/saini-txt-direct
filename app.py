b45700dc20057a077b64dd10383e1c6162471d38
rambhakt1-star <rambhakthoonme@gmail.com>
2025-08-30 19:24:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,46 +1,84 @@
 from flask import Flask
+
+import os
+
 app = Flask(__name__)
 
 @app.route('/')
+
 def hello_world():
+
     return """
+
 <!DOCTYPE html>
+
 <html lang="en">
 
 <body>
+
     <div class="container" style="bg-dark text-red text-center py-3 mt-5">
-        <a href="https://github.com/nikhilsainiop" class="card">
+
+        <a href="#" class="card">
+
             <p>
-	    <center>
-	        <br
-              /><br
-              />▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄<br 
-	      />██░▄▄▄░█░▄▄▀█▄░▄██░▀██░█▄░▄██<br 
-              />██▄▄▄▀▀█░▀▀░██░███░█░█░██░███<br 
-	      />██░▀▀▀░█░██░█▀░▀██░██▄░█▀░▀██<br 
-              />▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀<br 
-	      /><br
-              /><br>
-                <b>Powered By SAINI BOTS</b>
+
+	    <center>	        <br /><br />
+
+<pre style="font-size:18px; line-height:20px;">
+
+███████╗ ██████╗ ███╗   ██╗██╗   ██╗
+
+██╔════╝██╔═══██╗████╗  ██║██║   ██║
+
+███████╗██║   ██║██╔██╗ ██║██║   ██║
+
+╚════██║██║   ██║██║╚██╗██║██║   ██║
+
+███████║╚██████╔╝██║ ╚████║╚██████╔╝
+
+╚══════╝ ╚═════╝ ╚═╝  ╚═══╝ ╚═════╝ 
+
+</pre>
+
+	        <br /><br />
+
 		</center>
+
             </p>
+
         </a>
+
     </div>
+
 	<br></br>
+
         <center>
+
 	<footer class="bg-dark text-white text-center py-3 mt-5">
+
 		<div class="footer__copyright">
+
             <p class="footer__copyright-info">
+
                 © 2025 Video Downloader. All rights reserved.
+
             </p>
+
         </div>
+
     </footer>
+
     </center>
+
 </body>
 
 </html>
+
 """
 
-
 if __name__ == "__main__":
-    app.run()
+
+    port = int(os.environ.get("PORT", 5000))
+
+    app.run(host="0.0.0.0", port=port)
+    
